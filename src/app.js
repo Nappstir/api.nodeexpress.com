b9ac8b114c55329e657b6a0d0cbb9751ef95cd22
Travis Siebenhaar <tsiebenhaar@UT-MBP-TSIEBENHAAR.local>
2017-08-21 23:24:26 -0600

    WIP

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import apiRoutes from './routes/index';
+import compression from 'compression';
+import helmet from 'helmet';
 
 const app = express();
 const port = 4040;
 
+// compress all routes
+app.use(compression());
+
 // parse body params and attach them to req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
 
 app.use('/api', apiRoutes);
 
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,12 @@ import * as User from '../models/userModels';
 export function getUser(req, res, next) {
   User.getUser(req)
     .then(user => {
+      if (!user) {
+        res.status(404).json({error: 'Unauthorized access.'});
+      }
       res.json(user);
+    })
+    .catch(error => {
+      res.json({error})
     });
 }
