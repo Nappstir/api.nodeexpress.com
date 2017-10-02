53c185ccff69fbf212ce0ad1d2e1a7f34723e4f9
Travis Siebenhaar <tsiebenhaar@UT-MBP-TSIEBENHAAR.local>
2017-10-02 13:18:09 -0600

    Add babel-register for es6 in seed files. Tests now passing.

--- seeds/test/userSeed.js
+++ seeds/test/userSeed.js
@@ -1,28 +1,33 @@
+import hashPassword from '../../src/helpers/hashPassword';
+
 exports.seed = function(knex, Promise) {
-  return knex('users')
-    .then(function() {
-      return Promise.all([
-        // Insert seed entries
-        knex('users').insert([
-          {
-            first_name: 'admin',
-            last_name: 'admin',
-            email: 'admin@admin.com',
-            password: 'test'
-          },
-          {
-            first_name: 'test',
-            last_name: 'test',
-            email: 'test@test.com',
-            password: 'test'
-          },
-          {
-            first_name: 'Travis',
-            last_name: 'Siebenhaar',
-            email: 'trsiebenhaar@gmail.com',
-            password: 'test'
-          }
-        ]),
-      ]);
-    });
+  return knex('users').truncate()
+    .then(() => {
+      return knex('users')
+        .then(() => {
+          return Promise.all([
+            // Insert seed entries
+            knex('users').insert([
+              {
+                first_name: 'admin',
+                last_name: 'admin',
+                email: 'admin@admin.com',
+                password: hashPassword('test')
+              },
+              {
+                first_name: 'test',
+                last_name: 'test',
+                email: 'test@test.com',
+                password: hashPassword('test')
+              },
+              {
+                first_name: 'Travis',
+                last_name: 'Siebenhaar',
+                email: 'trsiebenhaar@gmail.com',
+                password: hashPassword('test')
+              }
+            ]),
+          ]);
+        });
+    })
 };
