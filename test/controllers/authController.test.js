4192e88db8a628d8ad01a64dd1f871f2c0fa2d10
Travis Siebenhaar <tsiebenhaar@UT-MBP-TSIEBENHAAR.local>
2017-10-03 13:09:54 -0600

    Test refactors.

--- test/controllers/authController.test.js
+++ test/controllers/authController.test.js
@@ -6,119 +6,120 @@ chai.use(chaiHttp);
 
 describe('Authentication Controller', () => {
 
-  describe('login success', () => {
+  describe('Login', () => {
+    describe('success', () => {
 
-    it('Responds with http status(200)', done => {
-      chai.request(server)
-        .post('/api/auth/login')
-        .send({
-          email: 'test@test.com',
-          password: 'test'
-        })
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          done();
-        });
+      it('Responds with http status(200)', done => {
+        chai.request(server)
+          .post('/api/auth/login')
+          .send({
+            email: 'test@test.com',
+            password: 'test'
+          })
+          .end((err, res) => {
+            expect(res).to.have.status(200);
+            done();
+          });
+      });
+
+      it('Responds with err(null)', done => {
+        chai.request(server)
+          .post('/api/auth/login')
+          .send({
+            email: 'test@test.com',
+            password: 'test'
+          })
+          .end((err, res) => {
+            expect(err).to.be.null;
+            done();
+          });
+      });
+
+      it('Responds with res.body(token)', done => {
+        chai.request(server)
+          .post('/api/auth/login')
+          .send({
+            email: 'test@test.com',
+            password: 'test'
+          })
+          .end((err, res) => {
+            expect(res.body).to.have.property('token');
+            done();
+          });
+      });
+
+      it('Responds with success(true)', done => {
+        chai.request(server)
+          .post('/api/auth/login')
+          .send({
+            email: 'test@test.com',
+            password: 'test'
+          })
+          .end((err, res) => {
+            expect(res.body.success).to.equal(true);
+            done();
+          });
+      });
+
+
     });
+    describe('failure', () => {
 
-    it('Responds with err(null)', done => {
-      chai.request(server)
-        .post('/api/auth/login')
-        .send({
-          email: 'test@test.com',
-          password: 'test'
-        })
-        .end((err, res) => {
-          expect(err).to.be.null;
-          done();
-        });
+      it('Responds with http status(401)', done => {
+        chai.request(server)
+          .post('/api/auth/login')
+          .send({
+            email: 'fake@news.com',
+            password: 'test'
+          })
+          .end((err, res) => {
+            expect(res).to.have.status(401);
+            done();
+          });
+      });
+
+      it('Responds with success(false)', done => {
+        chai.request(server)
+          .post('/api/auth/login')
+          .send({
+            email: 'fake@news.com',
+            password: 'test'
+          })
+          .end((err, res) => {
+            expect(res.body.success).to.equal(false);
+            done();
+          });
+      });
+
+      it('Responds with res.body(!token)', done => {
+        chai.request(server)
+          .post('/api/auth/login')
+          .send({
+            email: 'fake@news.com',
+            password: 'test'
+          })
+          .end((err, res) => {
+            expect(res.body).to.not.have.property('token');
+            done();
+          });
+      });
+
+      it('Responds with err(!null)', done => {
+        chai.request(server)
+          .post('/api/auth/login')
+          .send({
+            email: 'fake@news.com',
+            password: 'test'
+          })
+          .end((err) => {
+            expect(err).to.not.be.null;
+            done();
+          });
+      });
+
+
+
     });
-
-    it('Responds with res.body(token)', done => {
-      chai.request(server)
-        .post('/api/auth/login')
-        .send({
-          email: 'test@test.com',
-          password: 'test'
-        })
-        .end((err, res) => {
-          expect(res.body).to.have.property('token');
-          done();
-        });
-    });
-
-    it('Responds with success(true)', done => {
-      chai.request(server)
-        .post('/api/auth/login')
-        .send({
-          email: 'test@test.com',
-          password: 'test'
-        })
-        .end((err, res) => {
-          expect(res.body.success).to.equal(true);
-          done();
-        });
-    });
-
-
-  });
-
-  describe('login failure', () => {
-
-    it('Responds with http status(401)', done => {
-      chai.request(server)
-        .post('/api/auth/login')
-        .send({
-          email: 'fake@news.com',
-          password: 'test'
-        })
-        .end((err, res) => {
-          expect(res).to.have.status(401);
-          done();
-        });
-    });
-
-    it('Responds with success(false)', done => {
-      chai.request(server)
-        .post('/api/auth/login')
-        .send({
-          email: 'fake@news.com',
-          password: 'test'
-        })
-        .end((err, res) => {
-          expect(res.body.success).to.equal(false);
-          done();
-        });
-    });
-
-    it('Responds with res.body(!token)', done => {
-      chai.request(server)
-        .post('/api/auth/login')
-        .send({
-          email: 'fake@news.com',
-          password: 'test'
-        })
-        .end((err, res) => {
-          expect(res.body).to.not.have.property('token');
-          done();
-        });
-    });
-
-    it('Responds with err(!null)', done => {
-      chai.request(server)
-        .post('/api/auth/login')
-        .send({
-          email: 'fake@news.com',
-          password: 'test'
-        })
-        .end((err) => {
-          expect(err).to.not.be.null;
-          done();
-        });
-    });
-
-
-
   });
 
 });
--- test/models/userModel.test.js
+++ test/models/userModel.test.js
@@ -3,46 +3,47 @@ import * as User from '../../src/models/userModels';
 
 describe('User Model', () => {
 
-  describe('#getUser success', () => {
-    const req = {params: {id: 1}};
+  describe('#getUser', () => {
+    describe('success', () => {
+      const req = {params: {id: 1}};
 
-    it('Responds with object', done => {
-      User.getUser(req)
-        .then((res) => {
-          expect(res).to.be.an('object');
-          done();
-        });
+      it('Responds with object', done => {
+        User.getUser(req)
+          .then((res) => {
+            expect(res).to.be.an('object');
+            done();
+          });
+      });
+
+      it('Responds with res.id = req.params.id', done => {
+        User.getUser(req)
+          .then((res) => {
+            expect(res.id).to.be.equal(req.params.id);
+            done();
+          });
+      });
+
+      it('Responds with res.first_name = admin', done => {
+        User.getUser(req)
+          .then((res) => {
+            expect(res.first_name).to.be.equal('admin');
+            done();
+          });
+      });
+
     });
+    describe('failure', () => {
+      const req = {params: {id: 4}};
 
-    it('Responds with res.id = req.params.id', done => {
-      User.getUser(req)
-        .then((res) => {
-          expect(res.id).to.be.equal(req.params.id);
-          done();
-        });
+      it('Responds with err(null)', done => {
+        User.getUser(req)
+          .then((err, res) => {
+            expect(err).to.not.be.null;
+            done();
+          });
+      });
+
     });
-
-    it('Responds with res.first_name = admin', done => {
-      User.getUser(req)
-        .then((res) => {
-          expect(res.first_name).to.be.equal('admin');
-          done();
-        });
-    });
-
-  });
-
-  describe('#getUser failure', () => {
-    const req = {params: {id: 4}};
-
-    it('Responds with err(null)', done => {
-      User.getUser(req)
-        .then((err, res) => {
-          expect(err).to.not.be.null;
-          done();
-        });
-    });
-
   });
 
 
